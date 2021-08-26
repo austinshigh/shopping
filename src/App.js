c415d3ebc2eb4336224ba0ff3d473d11101f5f62
Austin High <austinhigh@Austins-MacBook-Pro.local>
2021-08-26 16:43:56 -0400

    Added router component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import { Route, BrowserRouter } from 'react-router-dom';
 import './App.css';
 import { Homepage } from './components/homepage/homepage.component';
+
+const HomePage = () =>
+    <div>
+        <Homepage/>
+    </div>
+
+const TopicsList = (props) =>
+    <div>
+        <button onClick={() => props.history.push('/topics/1')}> Topics </button>
+        <h1> TOPIC LIST</h1>
+    </div>
+
+const TopicDetail = (props) =>
+    <div>
+        <h1> TOPIC DETAIL: {props.match.params.topicId}</h1>
+    </div>
 
 function App() {
   return (
     <div className="App">
-      <Homepage/>
+        <BrowserRouter>
+            <Route exact path="/" component={ Homepage } />
+            <Route exact path="/topics" component={ TopicsList } />
+            <Route exact path="/topics/:topicId" component={ TopicDetail } />
+        </BrowserRouter>
     </div>
   );
 }
