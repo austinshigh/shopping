06f16edfe86f9bd873da5e07aaa923072a76be8a
Austin High <austinhigh@Austins-MacBook-Pro.local>
2021-08-27 07:18:52 -0400

    updated app with shop page, collection-preview and collection-item component

--- src/App.js
+++ src/App.js
@@ -1,31 +1,15 @@
 import React from 'react';
 import { Route, BrowserRouter } from 'react-router-dom';
 import './App.css';
-import { Homepage } from './components/homepage/homepage.component';
-
-const HomePage = () =>
-    <div>
-        <Homepage/>
-    </div>
-
-const TopicsList = (props) =>
-    <div>
-        <button onClick={() => props.history.push('/topics/1')}> Topics </button>
-        <h1> TOPIC LIST</h1>
-    </div>
-
-const TopicDetail = (props) =>
-    <div>
-        <h1> TOPIC DETAIL: {props.match.params.topicId}</h1>
-    </div>
+import { Homepage } from './pages/homepage/homepage.component';
+import { ShopPage } from './pages/shop/shop.component';
 
 function App() {
   return (
     <div className="App">
         <BrowserRouter>
             <Route exact path="/" component={ Homepage } />
-            <Route exact path="/topics" component={ TopicsList } />
-            <Route exact path="/topics/:topicId" component={ TopicDetail } />
+            <Route exact path="/shop" component={ ShopPage } />
         </BrowserRouter>
     </div>
   );
