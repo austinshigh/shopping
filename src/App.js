7dfdf98b9f2f8c2a8951f227435404a6dddf6465
Austin High <austinhigh@Austins-MacBook-Pro.local>
2021-08-31 10:37:13 -0400

    added firebase utils, included authentication for users into firestore via email/password combos

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ import  Homepage  from './pages/homepage/homepage.component';
 import  ShopPage  from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
-import { auth } from './firebase/firebase.utils.js';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import { onSnapshot } from "firebase/firestore";
 
 class App extends React.Component {
     constructor(){
@@ -21,8 +22,24 @@ class App extends React.Component {
     unsubscribeFromAuth = null;
 
     componentDidMount() {
-        this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-            this.setState({ currentUser: user })
+        this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+            if (userAuth){
+                const userRef = await createUserProfileDocument(userAuth);
+
+                onSnapshot((userRef), snapshot => {
+                    this.setState({
+                        currentUser: {
+                            id: snapshot.id,
+                            ...snapshot.data()
+                        }
+                    },
+                        () => {
+                        console.log(this.state);
+                    })
+                });
+
+            }
+            this.setState( { currentUser: userAuth });
         });
     }
 
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore/lite';
+import { getFirestore } from 'firebase/firestore';
+import { doc, getDoc, setDoc } from 'firebase/firestore';
 // Follow this pattern to import other Firebase services
 // import { } from 'firebase/<service>';
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
@@ -11,6 +12,30 @@ const firebaseConfig = {
     storageBucket: "storefront-dfe37.appspot.com",
     messagingSenderId: "719230128073",
     appId: "1:719230128073:web:0977a16264f2f310bd6218"
+};
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+
+    const userRef = doc(db, "users", `${userAuth.uid}`)
+    const docSnap = await getDoc(userRef);
+
+    if (!docSnap.exists()) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await setDoc((userRef), {
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            });
+        } catch (error) {
+            console.log('error creating user', error.message);
+        }
+     }
+    return userRef;
 };
 
 const app = initializeApp(firebaseConfig);
